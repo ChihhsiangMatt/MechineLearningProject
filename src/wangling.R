f7d4cd9d0f60354924206ce0118e7acd66a9705d
FrankXuefengXu <xuefeng.xu@wisc.edu>
2018-12-02 16:34:42 -0600

    Change Data Wrangling

--- src/wangling.R
+++ src/wangling.R
@@ -1,7 +1,20 @@
 require(tidyverse)
 require(dplyr)
 require(foreign)
-require(psych)
+require(ggplot2)
 
-movement <- read.csv('./data/wber.csv')
-upc <- read.csv('./data/upcber.csv')
+movement <- read_csv('./data/wber.csv')
+upc <- read_csv('./data/upcber.csv')
+store <- read.dta("./data/demo.dta")
+
+colnames(upc) <- c("code", "upc", "desc", "size", "case", "nitem")
+
+colnames(movement) <- c("store", "upc", "week", "move", "qty", "price", "prom", "profit", "valid", "priceHex", "profitHex")
+movement <- movement %>% 
+  filter(valid == 1) %>% 
+  select(-c("valid", "priceHex","profitHex")) %>% 
+  mutate(sales = price * move / qty)
+movement$prom <- as.character(movement$prom)
+movement$prom[movement$prom == ""] <- 'N'
+
+store <- store %>% select(c("name", "city", "zip", "store", "age9", "age60", "ethnic", "educ", "nocar", "income"))
